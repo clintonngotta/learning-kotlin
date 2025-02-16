4a62c2f7d4cc08c6e82490bab33c2a400c8f4381
clintonngotta <clintonngotta@gmail.com>
2025-02-16 09:25:56 +0300

    string templates

--- learnin-core-kotlin/src/Main.kt
+++ learnin-core-kotlin/src/Main.kt
@@ -8,32 +8,15 @@ const val PI = 3.14
 
 fun main() {
 
+    val name: String = "Learning Kotlin"
+    var age =20
+    println(name)
+    println("$name $age yrs old")
+    println(name.length) // lenght of letters
+    println(name.uppercase()) // prints uppercase
+    println(name.lowercase()) // prints lowercase
+    println(name[0]) // prints first letter
+    println(name.isEmpty()) // check if string is empty
 
-    var number: Int = 10     // integers
-
-    var long: Long =100L // long for longer integers/numbers
-
-    val double: Double = 3.2 //double for shorter decimals
-
-    var float: Float = 3.44F // float for longer decimals
-
-    var b: Boolean = true // true/false
-
-    var s: String = "Clinton"
-
-    var c: Char = 'C' // for single characters, should be in single quotes
-
-    var any: Any = 10 // any datatype, declare specific data types
-
-    var name: String? = "Learning" // declare nullable variable
-    name = "null safety"
-    println(name?.uppercase()) // prints uppercase if not null and null if null
-
-
-    var course: String? = "Kotlin" // declare nullable variable
-    course = null
-    println(course?.uppercase()) // prints null
-    // variable types can be inferred
-    // var mutable, val immutable
 
 }
